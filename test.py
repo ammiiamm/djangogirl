a62afacc8cd5ea3056de16dc4b609a0cc792206b
ammiiamm <contact.ammii@gmail.com>
2018-03-24 11:23:01 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,5 @@
+print('hello world')
+if 5 > 2:
+    print('True')
+else:
+    print('False')
---
+++ test2.py
@@ -0,0 +1,16 @@
+def hi(name):
+    if name == "ammii":
+        print('Hi, ' + name)
+    elif name == "am":
+        print('Hello, ' + name)
+    else:
+        print('Sawasdee '+ name)
+    return 'RETURN from hi()'
+
+
+t = hi("ammiiam")
+print(t)
+
+arr = [1,2,3,4,5]
+for a in arr:
+    print(a)
